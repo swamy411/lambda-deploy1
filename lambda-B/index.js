3ff14644cabb741b045f2e9d8cbea3f0d6513406
swamy411 <bhairavswamyvijjapu@gmail.com>
2021-03-15 20:34:17 +0530

    5

--- lambda-B/index.js
+++ lambda-B/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-B!-Updated '),
+        body: JSON.stringify('Hello from Lambda-B! '),
     };
     return response;
 };
