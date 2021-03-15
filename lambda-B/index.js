89aaaedf1d26d796415a29e07de0fc8d1c1d8fcd
swamy411 <bhairavswamyvijjapu@gmail.com>
2021-03-15 18:04:50 +0530

    second commit

--- lambda-B/index.js
+++ lambda-B/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-B!-Updated to new change'),
+        body: JSON.stringify('Hello from Lambda-B!-Updated to new change second'),
     };
     return response;
 };
