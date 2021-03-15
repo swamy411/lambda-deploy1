22e7d5a1250945e9473033d086ec39a65b02c836
swamy411 <bhairavswamyvijjapu@gmail.com>
2021-03-16 00:25:27 +0530

    new

--- lambda-D/index.js
+++ lambda-D/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-D! - Updated Version '),
+        body: JSON.stringify('Hello from Lambda-D! - Updated Version 2ndtime'),
     };
     return response;
 };
