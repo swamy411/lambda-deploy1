45fd127755c3f4a43e2fa9075d0dea5b54f744df
swamy411 <bhairavswamyvijjapu@gmail.com>
2021-03-16 00:19:27 +0530

    updated commit

--- lambda-C/index.js
+++ lambda-C/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-C! updated'),
+        body: JSON.stringify('Hello from Lambda-C! newly updated'),
     };
     return response;
 };
