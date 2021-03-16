5e23ae97cee4c64cc7ea0d0029bc72409f1bf023
swamy411 <bhairavswamyvijjapu@gmail.com>
2021-03-16 13:05:52 +0530

    5

--- lambda-C/index.js
+++ lambda-C/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-C! newly updated 4nd time'),
+        body: JSON.stringify('Hello from Lambda-C! newly updated 5nd time'),
     };
     return response;
 };
