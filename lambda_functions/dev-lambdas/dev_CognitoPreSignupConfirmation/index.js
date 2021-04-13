2e7fb684017a514b15dd8305aaa9d193cf8ec7d6
swamy411 <vijjapubhairavaswamy@gmail.com>
2021-04-13 20:39:39 +0530

    test jenkins

--- lambda_functions/dev-lambdas/dev_CognitoPreSignupConfirmation/index.js
+++ lambda_functions/dev-lambdas/dev_CognitoPreSignupConfirmation/index.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
   console.log(event, context, callback);
   console.log(event.triggerSource);
   if (event.request.userAttributes.hasOwnProperty('custom:group_name')) {
-      // if(event.request.userAttributes['custom:group_name'] == 'admin') {
+      // if(event.request.userAttributes['custom:group_name'] == 'admin'){
       if(event.request.userAttributes['custom:group_name'] != 'surgeon' && event.request.userAttributes['custom:group_name'] != 'hospital_admin') {
           callback(Error('errorregistration'));
       }
